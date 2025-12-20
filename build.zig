dafe3a72fe8b8121fed1eaed98d197ed4a927edc
Squibid <me@zacharyscheiman.com>
2025-12-19 23:46:22 -0500

    luajit was crashing mez without erroring

--- build.zig
+++ build.zig
@@ -43,7 +43,7 @@ pub fn build(b: *std.Build) void {
   const xkbcommon = b.dependency("xkbcommon", .{}).module("xkbcommon");
   const pixman = b.dependency("pixman", .{}).module("pixman");
   const wlroots = b.dependency("wlroots", .{}).module("wlroots");
-  const zlua = b.dependency("zlua", .{ .lang = .luajit }).module("zlua");
+  const zlua = b.dependency("zlua", .{ .lang = .lua51 }).module("zlua");
   const clap = b.dependency("clap", .{}).module("clap");
 
   wlroots.addImport("wayland", wayland);
