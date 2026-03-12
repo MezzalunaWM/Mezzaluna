ec9f7f984a1bb832979b37ef3f8bda30c4f0010b
Squibid <me@zacharyscheiman.com>
2026-03-12 19:41:23 -0400

    fix the runtime init.lua code - plugins now load from ~/.local/share/mez/plugins instead of ~/.local/share/plugins - remove duplicate config path resolution code

--- runtime/init.lua
+++ runtime/init.lua
@@ -1,12 +1,16 @@
 ---@module "mez_types"
 
-local env_conf = os.getenv("XDG_CONFIG_HOME")
-if not env_conf then
-  env_conf = os.getenv("HOME")
+-- don't find the config directory if one was provided already
+if not mez.path.config then
+  local env_conf = os.getenv("XDG_CONFIG_HOME")
   if not env_conf then
-    error("Couldn't determine potential config directory is $HOME set?")
+    env_conf = os.getenv("HOME")
+    if not env_conf then
+      error("Couldn't determine potential config directory is $HOME set?")
+    end
+    env_conf = mez.fs.joinpath(env_conf, ".config")
   end
-  env_conf = mez.fs.joinpath(env_conf, ".config")
+  mez.path.config = mez.fs.joinpath(env_conf, "mez")
 end
 
 local env_data = os.getenv("XDG_DATA_HOME")
@@ -16,12 +20,12 @@ if not env_data then
     error("Couldn't determine potential data directory is $HOME set?")
   end
 
-  env_data = mez.fs.joinpath(env_data, ".local", "share", "mez")
+  env_data = mez.fs.joinpath(env_data, ".local", "share")
 end
 
 -- allow plugin loading in .local/share/mez/plugins
-local plugin_dir = mez.fs.joinpath(env_data, "plugins")
-
+local plugin_dir = mez.fs.joinpath(env_data, "mez", "plugins")
+-- TODO: we should make a function for this in mez.fs instead of using the shell
 os.execute("mkdir -p " .. plugin_dir)
 for _, plugin_name in ipairs(mez.fs.subdirs(plugin_dir)) do
   package.path = package.path .. ";" .. mez.fs.joinpath(plugin_dir, plugin_name, "lua", "?", "init.lua")
@@ -31,23 +35,11 @@ end
 -- allow loading files in the runtime directory
 package.path = package.path .. ";" .. mez.fs.joinpath(mez.path.runtime, "?.lua")
 
+-- add the inspect function to mez
 mez.inspect = require("inspect").inspect
 
+-- setup the base_config and config paths to be loaded through zig
 mez.path.base_config = mez.fs.joinpath(mez.path.runtime, "base_config.lua")
-
-if not mez.path.config then
-  local env_conf = os.getenv("XDG_CONFIG_HOME")
-
-  if not env_conf then
-    env_conf = os.getenv("HOME")
-    if not env_conf then
-      error("Couldn't determine potential config directory is $HOME set?")
-    end
-    env_conf = mez.fs.joinpath(env_conf, ".config")
-  end
-
-  mez.path.config = mez.fs.joinpath(env_conf, "mez")
-end
 
 package.path = package.path..";"..mez.fs.joinpath(mez.path.config, "lua", "?.lua")
 mez.path.config = mez.fs.joinpath(mez.path.config, "init.lua")
