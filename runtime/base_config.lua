3ae1204cb9b20389f934b24ff5721caeb03e7324
Harrison DiAmbrosio <hdiambrosio@gmail.com>
2026-03-12 13:04:34 -0400

    added output scaling

--- runtime/base_config.lua
+++ runtime/base_config.lua
@@ -4,8 +4,11 @@ local mod = "alt"
 local terminal_emulator = "foot"
 local run_launcher = "wmenu-run"
 
-local master = require("master")
-master.setup({ mod_key = mod })
+-- local master = require("master")
+-- master.setup({ mod_key = mod })
+
+local euclid = require("euclid")
+euclid.setup({ mod_key = mod })
 
 local border_width = 4
 
