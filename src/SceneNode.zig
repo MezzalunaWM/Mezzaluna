922c013cee84757a60faa07696183a478e1d4870
Squibid <me@zacharyscheiman.com>
2026-05-12 13:42:38 -0400

    make the scene node iterator do correct recusion

--- src/SceneNode.zig
+++ src/SceneNode.zig
@@ -21,17 +21,13 @@ pub fn Iterator(comptime direction: wl.list.Direction) type {
         node_iter: wl.list.Head(wlr.SceneNode, .link).Iterator(direction),
         i: u32,
         pub fn next(self: *@This()) ?*Data {
-            std.debug.assert(@intFromPtr(self.trees[self.i]) != 0);
-            if (self.trees[self.i].children.length() == 0) return self.next();
-
             while (self.node_iter.next()) |node| {
-                if (node.data == null) continue;
+                std.debug.assert(node.data != null);
                 return @ptrCast(@alignCast(node.data.?));
             }
 
             self.i += 1;
             if (self.i >= self.trees.len) return null;
-
             self.node_iter = self.trees[self.i].children.iterator(direction);
             return self.next();
         }
